415ea0d4e9b95dbd58579fa5e7b1bec5b33b2cc2
hamdi <hamdi_1_4_97@hotmail.com>
2018-07-21 11:42:32 +0300

    Relaionship create

--- app/Model/Prodect.php
+++ app/Model/Prodect.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prodect extends Model
 {
-    //
+    public function reviews()
+    {
+    	return $this->hasMany('App\Model\Review');
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
-}
+    public function prodect()
+    {
+    	return $this->belongsTo('App\Model\Prodect');
+    }}
